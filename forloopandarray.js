c58ce6d4784c415ce53e845742c736fb6b791554
9dbartlett <31480988+9dbartlett@users.noreply.github.com>
2017-09-27 10:11:55 -0500

    Add files via upload

---
+++ forloopandarray.js
@@ -0,0 +1,43 @@
+let oz = [8,12,16,20,24,32];
+let sizes = ["xs","s","m","l",'xl'];
+let prices = [4.99,9.99,19.99,49.99];
+let speeds = [93,99,95,96,97,99];
+let pings = [45,60,120,80,155,160,220,110];
+let bits = [1,1,0,1,0,0,1,0,0,1];
+
+function ounces(oz){
+    let 24 =[];
+    for(let d=0;d<oz.length;d++){
+        if (oz [d] >=amount){
+            24 oz.push(oz[d])
+        }
+}
+    return 24 oz;
+}
+
+function example2(){
+    let total=0;
+    for(let b=0;b<bits.length;b++){
+        total = total+ bits[b];
+        //new = old + current #
+    }
+    return total;
+}
+
+function example3(){
+
+}
+
+function example4(){
+
+}
+
+//Creating test code
+function main(){
+    console.log(example1());
+    console.log("The total # of bits is "+example2());
+    console.log("Your average ping for internet is "+example3());
+    console.log("The first pitch thrown above 95 was pitch-index "+example4());
+}
+//run test code with CTRL + SHIFT + B
+main();
