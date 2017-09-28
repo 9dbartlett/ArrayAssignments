ddbba41760c5548bf6f9d18c52ba61cbeb47df86
9dbartlett <31480988+9dbartlett@users.noreply.github.com>
2017-09-28 09:19:27 -0500

    Add files via upload

--- forloopandarray.js
+++ forloopandarray.js
@@ -5,39 +5,44 @@ let speeds = [93,99,95,96,97,99];
 let pings = [45,60,120,80,155,160,220,110];
 let bits = [1,1,0,1,0,0,1,0,0,1];
 
-function ounces(oz){
-    let 24 =[];
-    for(let d=0;d<oz.length;d++){
-        if (oz [d] >=amount){
-            24 oz.push(oz[d])
+function example1(list,percent){
+    let  =[];
+    for(let d=0; d<oz.length; d++){
+        if (example1 [d] >=amount){
+            example1.push(ozs[d])
         }
 }
-    return 24 oz;
+    return example1;
 }
 
-function example2(){
+function bits(bit){
     let total=0;
-    for(let b=0;b<bits.length;b++){
+    for(let b=0; b<bit.length; b++){
         total = total+ bits[b];
         //new = old + current #
     }
     return total;
 }
 
-function example3(){
+function average(list){
 
+    let count = 0;
+    for(let c=0; c<list.length; c++){
+        count=count + list[c]
+    }
+return count/list.length
 }
 
-function example4(){
+function example4(list, number){
 
 }
 
 //Creating test code
 function main(){
-    console.log(example1());
+    console.log(example1(oz,20));
     console.log("The total # of bits is "+example2());
-    console.log("Your average ping for internet is "+example3());
-    console.log("The first pitch thrown above 95 was pitch-index "+example4());
+    console.log("Your average ping for internet is "+average(pings));
+    console.log("The first pitch thrown above 95 was pitch-index "+example4(speeds, 95));
 }
 //run test code with CTRL + SHIFT + B
 main();
